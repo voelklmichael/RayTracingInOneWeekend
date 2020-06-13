1cc5ca1cf7a2dd5e131087537f201c277da7f70f
Michael <v.mi@gmx.de>
2020-06-13 21:14:04 +0200

    checking gitignore

--- target/debug/deps/raytracinginoneweekend-e409c68d87ed9e5a.d
+++ target/debug/deps/raytracinginoneweekend-e409c68d87ed9e5a.d
@@ -1,8 +1,9 @@
-/home/michael/raytracinginoneweekend/RayTracingInOneWeekend/target/debug/deps/raytracinginoneweekend-e409c68d87ed9e5a: src/main.rs src/bmp_converter.rs src/rbg.rs src/types.rs
+/home/michael/raytracinginoneweekend/RayTracingInOneWeekend/target/debug/deps/raytracinginoneweekend-e409c68d87ed9e5a: src/main.rs src/bmp_converter.rs src/rbg.rs src/types.rs src/vec3.rs
 
-/home/michael/raytracinginoneweekend/RayTracingInOneWeekend/target/debug/deps/raytracinginoneweekend-e409c68d87ed9e5a.d: src/main.rs src/bmp_converter.rs src/rbg.rs src/types.rs
+/home/michael/raytracinginoneweekend/RayTracingInOneWeekend/target/debug/deps/raytracinginoneweekend-e409c68d87ed9e5a.d: src/main.rs src/bmp_converter.rs src/rbg.rs src/types.rs src/vec3.rs
 
 src/main.rs:
 src/bmp_converter.rs:
 src/rbg.rs:
 src/types.rs:
+src/vec3.rs:
